d768153663121f7696d41192b0fb9b7855c735b0
Domen988 <zagar.domen@gmail.com>
2014-08-30 22:47:19 +0200

    Done!

---
+++ gui.py
@@ -0,0 +1,11 @@
+__author__ = 'Domen'
+
+
+from Tkinter import *
+
+frmMain = Tk()
+
+label = Label(frmMain, text="Welcome to py2exe!")
+label.pack()
+
+frmMain.mainloop()
---
+++ setup.py
@@ -0,0 +1,7 @@
+__author__ = 'Domen'
+
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['PDF-merger-for-DS.py'])
